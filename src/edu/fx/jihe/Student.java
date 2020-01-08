be273c3050a7120b0453a0cda26c4fc5daba6504
pxn <pu6422534@163.com>
2020-01-09 00:11:55 +0800

    常用类：比较器student

--- src/edu/fx/jihe/Student.java
+++ src/edu/fx/jihe/Student.java
@@ -6,7 +6,7 @@ import java.util.Objects;
  * @author: Pxn
  * @date: 2019/12/25 18:07
  */
-public class Student {
+public class Student  implements Comparable{
     private String name;
     private int age;
 
@@ -54,5 +54,21 @@ public class Student {
     @Override
     public int hashCode() {
         return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Student){//判断对象的类型是否是学生
+            Student student=(Student) o;
+            if (this.age>student.age){
+                return 1;
+            }else if(this.age<student.age){
+                return -1;
+            }else {
+                return -this.name.compareTo(student.name);
+            }
+        }else{
+            throw new RuntimeException("不是该学生类型对象！");
+        }
     }
 }
